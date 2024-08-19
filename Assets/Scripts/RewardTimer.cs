2debc493d441680302c22a376eef5e29c07d8e5d
KhizarApex <apexlogics5@gmail.com>
2024-08-19 12:01:16 +0500

    Reward Timer

--- Assets/Scripts/RewardTimer.cs
+++ Assets/Scripts/RewardTimer.cs
@@ -2,47 +2,84 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Events;
+using System;
 
 public class RewardTimer : MonoBehaviour
 {
+    private string timerKey;
     private Button button;
-    public float requiredTime = 30.0f;
+    public float requiredTime = 30.0f; // Time in seconds
     public TextMeshProUGUI timer;
     private bool timeOver = true;
     public UnityEvent timeFinished;
 
+    private DateTime timerStart;
+    private DateTime timerEnd;
+    private bool isTimerRunning = false;
+
     private void Awake()
     {
+        timerKey = gameObject.name + "_TimerEnd";
         timer.gameObject.SetActive(false);
         button = gameObject.GetComponent<Button>();
-        button.onClick.AddListener(startTimer);
-    }
-    private void startTimer() 
-    {
-        if(requiredTime >= 0) 
+        button.onClick.AddListener(StartTimer);
+
+        // Check if a timer was already running
+        if (PlayerPrefs.HasKey(timerKey))
         {
-            Debug.Log("helooooo");
-            timeOver = false;
+            timerEnd = DateTime.Parse(PlayerPrefs.GetString(timerKey));
+            double remainingTime = (timerEnd - DateTime.Now).TotalSeconds;
+
+            if (remainingTime > 0)
+            {
+                requiredTime = (float)remainingTime;
+                timeOver = false;
+                isTimerRunning = true;
+            }
+            else
+            {
+                timeFinished.Invoke();
+                requiredTime = 0;
+            }
         }
     }
+
+    private void StartTimer()
+    {
+        if (!isTimerRunning)
+        {
+            timeOver = false;
+            isTimerRunning = true;
+            timerStart = DateTime.Now;
+            timerEnd = timerStart.AddSeconds(requiredTime);
+
+            // Save timer end time in PlayerPrefs
+            PlayerPrefs.SetString(timerKey, timerEnd.ToString());
+            PlayerPrefs.Save();
+        }
+    }
+
     private void Update()
     {
-        if (!timeOver) 
+        if (!timeOver)
         {
             timer.gameObject.SetActive(true);
-            if(requiredTime >= 1) 
-            {
-                requiredTime -=Time.deltaTime;
-            }
+            double remainingTime = (timerEnd - DateTime.Now).TotalSeconds;
 
-            if(requiredTime < 1) 
+            if (remainingTime > 0)
+            {
+                requiredTime = (float)remainingTime;
+                DisplayTime(requiredTime);
+            }
+            else
             {
                 timeOver = true;
+                isTimerRunning = false;
+                requiredTime = 0;
+                DisplayTime(0);
                 timeFinished.Invoke();
-
-
+                PlayerPrefs.DeleteKey(timerKey);
             }
-            DisplayTime(requiredTime);
         }
     }
 
@@ -50,25 +87,15 @@ public class RewardTimer : MonoBehaviour
     {
         float minutes = Mathf.FloorToInt(timeLeft / 60);
         float seconds = Mathf.FloorToInt(timeLeft % 60);
-
-
-        timer.text = string.Format("{00:00} : {01:00}", minutes, seconds);
-
+        timer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
-
-
 
     private void OnApplicationQuit()
     {
-
-
-        Debug.Log("App Closed!");
-
-        //if (isTimerRunning)
-        //{
-        //    PlayerPrefs.SetString("TimerEnd", timerEnd.ToString());
-        //    PlayerPrefs.Save();
-        //}
+        if (isTimerRunning)
+        {
+            PlayerPrefs.SetString(timerKey, timerEnd.ToString());
+            PlayerPrefs.Save();
+        }
     }
-
 }
